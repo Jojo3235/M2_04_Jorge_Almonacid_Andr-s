f3c7357d9a48728c62b05a763db435a3759f7215
Jojo3235 <joralmand@gmail.com>
2022-10-04 21:04:29 +0200

    Initial commit

---
+++ Ejercicio1.py
@@ -0,0 +1,8 @@
+#Ejercicio 1
+colores_luz = ["azul", "rojo", "verde"]
+sólidos_platónicos = ("tetraedro", "cubo", "octaedro", "dodecaedro", "icosaedro")
+
+print(colores_luz)
+print(sólidos_platónicos)
+
+print("El primer elemento de la lista es:", colores_luz[1], "\nEl penúltimo elemento de la tupla es:", sólidos_platónicos[-2])
