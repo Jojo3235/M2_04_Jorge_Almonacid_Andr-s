1ddb5459d37ca571b4191b95fe3d25d663b8ee5a
Jojo3235 <joralmand@gmail.com>
2022-10-06 21:10:25 +0200

    ligeras modificaciones ejercicio 3 y 4

--- main.py
+++ main.py
@@ -79,14 +79,14 @@ print(clases)
 lista_numeros=list()   #Creamos una lista vacía
 i=0     #Creamos una variable y la igualamos a 0
 while i<3:  #Mientras que la i sea menor que 3 se reitera el proceso
-    i=i+1   #Sumamos 1 cada vez que se repita el bucle
+    i+=1   #Sumamos 1 cada vez que se repita el bucle
     numero1=float(input("Introduce un número: "))  #Pedimos que introduzca un número
     lista_numeros.append(numero1)  #Añadimos el número introducido a la lista
 else:
     pass            #Cuando i = 3 decimos que pase el bucle 
 sumatorio = sum(lista_numeros)      #Definimos la variable sumatorio como la suma de los términos de la lista
-print(sumatorio)        #Imprimimos el sumatorio
+print("La suma de todos los números es",sumatorio)        #Imprimimos el sumatorio
 
 #Ejercicio 4
 media=sumatorio/len(lista_numeros)  #Creamos una función llamada media en la que dividimos la suma entra la longitud de la lista
-print(media)    #Imprimimos la media
+print("La media de todos los números es",media)    #Imprimimos la media
